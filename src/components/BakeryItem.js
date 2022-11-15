31eda237710a89cf41f4f5c84fde86ae5ab540f8
MarkusChu <75825637+MarkusChu@users.noreply.github.com>
2022-11-15 06:03:36 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
+import "../App.css";
+
 // TODO: create a component that displays a single bakery item
+function BakeryItem(image, name, price) {
+    return (
+        <div className="BakeryItem">
+            <img src={image} alt={name} />
+            <p>{name}</p>
+            <p>${price}</p>
+        </div>
+    );
+}
+
+export default BakeryItem;
