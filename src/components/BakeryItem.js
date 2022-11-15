fbff7d60777bc3cab87bc176c326c30054713b11
MarkusChu <75825637+MarkusChu@users.noreply.github.com>
2022-11-15 06:27:07 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -5,7 +5,7 @@ function BakeryItem(props) {
     return (
         <div className="BakeryItem">
             <img src={props.image} alt={props.name} />
-            <p>${props.name}</p>
+            <p>{props.name}</p>
             <p>${props.price}</p>
         </div>
     );
