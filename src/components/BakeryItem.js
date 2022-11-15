9c7b90e0fbf121c4140436e016308a0e227fea5f
MarkusChu <75825637+MarkusChu@users.noreply.github.com>
2022-11-15 06:34:15 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -6,7 +6,9 @@ function BakeryItem(props) {
         <div className="BakeryItem">
             <img src={props.image} alt={props.name} />
             <p>{props.name}</p>
+            <p>{props.description}</p>
             <p>${props.price}</p>
+            <button onClick={() => props.callback(props)}>Add to Cart</button>
         </div>
     );
 }
